c417f4926a795d27d3520a08de39154b058dbcf7
Bohdan <bohdan.solomakha@gmail.com>
2022-04-21 21:44:24 +0300

    Some changes related to routes were added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
 import React, { FC } from "react";
 import "./App.css";
+import { useRoutes, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Event from "./pages/Event";
+
+export enum RouteNames {
+  LOGIN = "/login",
+  EVENT = "/",
+}
 
 const App: FC = (): JSX.Element => {
-  return <>Work</>;
+  const auth = true;
+  const element = useRoutes([
+    auth
+      ? {
+          path: RouteNames.EVENT,
+          element: <Event />,
+        }
+      : {
+          path: RouteNames.LOGIN,
+          element: <Login />,
+        },
+  ]);
+
+  return <div>{element}</div>;
 };
 
 export default App;

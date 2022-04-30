6b133e38be29ffc238cab3dd4ebd594298f159f5
Bohdan <bohdan.solomakha@gmail.com>
2022-04-30 18:09:33 +0300

    Added eventActions (hook for using actions without dispatch)

---
+++ src/redux/action-creators/eventActions.ts
@@ -0,0 +1,53 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { setEvents, setGuests, setIsPending } from "../reducers/eventSlice";
+import UserService from "../../api/UserService";
+import { IEvent } from "../../models/IEvent";
+
+export const fetchGuests = createAsyncThunk(
+  "fetch/Guests",
+  async (_, { dispatch }) => {
+    try {
+      setTimeout(async () => {
+        const res = await UserService.getUsers();
+        dispatch(setGuests(res.data));
+      }, 1000);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+);
+
+export const createEvent = createAsyncThunk(
+  "create/Event",
+  async (event: IEvent, { dispatch }) => {
+    try {
+      dispatch(setIsPending(true));
+      setTimeout(async () => {
+        const events = localStorage.getItem("event") || "[]";
+        const json = JSON.parse(events) as IEvent[];
+        json.push(event);
+        dispatch(setEvents(json));
+        localStorage.setItem("event", JSON.stringify(json));
+        dispatch(setIsPending(false));
+      }, 1000);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+);
+
+export const fetchEvents = createAsyncThunk(
+  "fetch/events",
+  async (username: string, { dispatch }) => {
+    try {
+      const events = localStorage.getItem("event") || "[]";
+      const jsonEv = JSON.parse(events) as IEvent[];
+      const filteredEv = jsonEv.filter(
+        (ev) => ev.author === username || ev.guest === username
+      );
+      dispatch(setEvents(filteredEv));
+    } catch (e) {
+      console.log(e);
+    }
+  }
+);
